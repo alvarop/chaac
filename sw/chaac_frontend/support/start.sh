45a92e520cf5fac8e523c8f515f5dd87a6c04811
Alvaro Prieto <source@alvaroprieto.com>
2019-01-10 05:42:22 -0800

    Don't need the python version specified

--- sw/chaac_frontend/support/start.sh
+++ sw/chaac_frontend/support/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export PIPENV_VENV_IN_PROJECT=true
-export DATABASE=/home/pi/wx_out1.db
+export DATABASE=/home/pi/chaac.db
 pipenv run gunicorn --workers 5 --bind unix:chaac_frontend.sock -m 007 src:app
