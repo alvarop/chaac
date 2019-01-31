4f07febc0b6a46f1919f118e480dca3d633095d1
Alvaro Prieto <source@alvaroprieto.com>
2019-01-30 16:45:37 -0800

    Added comment/todo to chaac_packet.py

--- sw/chaac/chaac/chaac_packet.py
+++ sw/chaac/chaac/chaac_packet.py
@@ -16,7 +16,9 @@ class ChaacPacket:
     def encode(self, args):
         return struct.pack(self.struct_str, *args)
 
-    def round(self, namedtuple):        
+    def round(self, namedtuple):
+        # Round all floating point items to 3 decimal places
+        # TODO - have precision and other options set in packet_struct
         data_dict = namedtuple._asdict()
         for key,val in data_dict.items():
             if isinstance(val, float):
