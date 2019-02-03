819685c97d4f3c2a4b8f36578aefe2157df65ca0
Alvaro Prieto <alvaro@Alvaros-MacBook-Pro.local>
2019-02-02 20:10:28 -0800

    Start plotting bar charts for rain!

--- sw/chaac_frontend/src/chaac_frontend.py
+++ sw/chaac_frontend/src/chaac_frontend.py
@@ -31,6 +31,7 @@ def close_db(error):
 
 @app.route("/")
 def summary():
+    """ Get latest weather data (and past 24 hour rainfall) """
 
     # Get last sample
     db = get_db()
@@ -57,8 +58,48 @@ def summary():
     return render_template("status.html", sample=sample)
 
 
-def get_json_str(start_date, end_date):
+rain_mod = {"day": (60 * 60), "week": (60 * 60 * 24), "month": (60 * 60 * 24)}
 
+
+def get_start_bin(end_date, table):
+    """ Figure out what time it is now to start the bar chart
+        The numbers depend on whether it's a day/week/month plot
+    """
+
+    # Start one day (% rain_mod) after today
+    end_date += rain_mod[table]
+    if table == "day":
+        return datetime.fromtimestamp(end_date).timetuple().tm_hour
+    elif table == "week":
+        return datetime.fromtimestamp(end_date).timetuple().tm_wday
+    elif table == "month":
+        return datetime.fromtimestamp(end_date).timetuple().tm_mday
+    else:
+        return None
+
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+
+def get_rain_label(idx, table):
+    """ Get nice labels for the bar chart. Unfortunately, plotly
+    keeps re-sorting all the numbers so we have to add strings
+    around them to keep it in the correct order. """
+
+    if table == "day":
+        return "(" + str(idx) + ")"
+    elif table == "week":
+        return days[idx]
+    elif table == "month":
+        return "(" + str(idx) + ")"
+    else:
+        return None
+
+
+def get_json_str(start_date, end_date):
+    """ Get weather data for the specified weather period """
+
+    # Figure out what data range we're dealing with
     td = int(end_date) - int(start_date)
     if td <= 60 * 60 * 24:
         table = "day"
@@ -83,6 +124,10 @@ def get_json_str(start_date, end_date):
     for name in col_names:
         plot[name] = []
 
+    # Rain doesn't have the same timestamp as the rest of the data
+    plot["rain_time"] = []
+
+    # Create lists with each data type and make timestamp pretty
     for row in rows:
         for name in col_names:
             if name == "timestamp":
@@ -91,10 +136,36 @@ def get_json_str(start_date, end_date):
                         "%Y-%m-%d %H:%M:%S"
                     )
                 )
-            elif name == "uid" or name == "id":
+            elif name == "uid" or name == "id" or name == "rain":
                 continue
             else:
                 plot[name].append(round(getattr(row, name), 3))
+
+    # TODO - set start date to the beginning of that day
+    # That way the bins are accurate to the day
+    
+    # Get rain data for the time period
+    rain_data = db.get_rain(int(start_date), int(end_date))
+    rain_total = 0
+
+    # Bin data into the appropriate size for histograms
+    idx = get_start_bin(int(end_date), table)
+    bins = range(int(start_date), int(end_date), rain_mod[table])
+    
+    # Loop through each rain bin
+    for rain_bin in bins:
+        plot["rain_time"].append(get_rain_label(idx, table))
+        rain = 0
+        # Loop through each rain sample
+        for rain_hour in rain_data:
+            # Check if the sample falls into our bin
+            if rain_hour[1] >= rain_bin and rain_hour[1] < (rain_bin + rain_mod[table]):
+                rain += rain_hour[3]
+
+        plot["rain"].append(rain)
+
+        # Wrap around depending on the number of bins (since we don't always start at 0)
+        idx = (idx + 1) % len(bins)
 
     return jsonify(plot)
 
