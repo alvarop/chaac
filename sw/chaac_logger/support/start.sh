f8d66854556e4e0b64a86141d8ab89dc660c04e9
Alvaro Prieto <source@alvaroprieto.com>
2019-02-09 11:38:58 -0800

    Update logger baud rate

--- sw/chaac_logger/support/start.sh
+++ sw/chaac_logger/support/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pipenv run python -u chaac_logger.py --port /dev/ttyS0 --baud_rate 115200 --db /home/pi/chaac.db
+pipenv run python -u chaac_logger.py --port /dev/ttyS0 --baud_rate 57600 --db /home/pi/chaac.db
