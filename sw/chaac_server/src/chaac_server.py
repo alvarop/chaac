420be57e297377d66dd22628e3ba2a97625d878c
Alvaro Prieto <alvaro@Alvaros-MacBook-Pro.local>
2019-09-23 06:51:11 -0700

    Enable cache results for chaac_server

--- sw/chaac_server/src/chaac_server.py
+++ sw/chaac_server/src/chaac_server.py
@@ -3,6 +3,8 @@ import os
 import time
 import socket
 import requests
+import base64
+import yaml
 from datetime import datetime, timedelta
 from flask import Flask, request, g, render_template, jsonify, Response
 
@@ -10,16 +12,30 @@ app = Flask(__name__)
 
 app.config.from_object(__name__)  # load config from this file , flaskr.py
 
-# Load default config and override config from an environment variable
-# app.config.update(dict(DATABASE=os.getenv("DATABASE")))
+config = None
+with open(os.getenv("CONFIG"), "r") as config_file:
+    config = yaml.safe_load(config_file)
 
-hosts = {"San Mateo": "10.1.1.10", "Merida": "10.1.1.11", "chaac-dev": "10.1.1.20"}
-
+if config is None:
+    raise Exception("Unable to load config file")
 
 def json_error(message):
     error = {}
     error["error"] = message
     return jsonify(error)
+
+def cached_result(host, expiration_s = 120):
+    if host not in config['hosts']:
+        print("Invalid host " + host)
+        return None
+
+    cache_filename = "{}/{}.yml".format(config["cache_dir"], host)
+    with open(cache_filename, 'r') as cache_file:
+        cache = yaml.safe_load(cache_file)
+        if (time.time() - cache['timestamp']) < expiration_s:
+             return base64.b64decode(cache['json']).decode('utf-8')
+
+    return None
 
 
 def json_from_url(url):
@@ -35,39 +51,43 @@ def json_from_url(url):
 
 @app.route("/<host>/latest")
 def latest_json(host):
-    if host not in hosts:
+    if host not in config['hosts']:
         return json_error("Invalid host")
 
-    return json_from_url("http://" + hosts[host] + "/latest")
+    cached_json = cached_result(host)
+    if cached_json:
+        return cached_json    
+    else:
+        return json_from_url("http://" + config['hosts'][host] + "/latest")
 
 
 @app.route("/<host>/json/day")
 def json_day_str(host):
-    if host not in hosts:
+    if host not in config['hosts']:
         return json_error("Invalid host")
 
-    return json_from_url("http://" + hosts[host] + "/json/day")
+    return json_from_url("http://" + config['hosts'][host] + "/json/day")
 
 
 @app.route("/<host>/json/week")
 def json_week_str(host):
-    if host not in hosts:
+    if host not in config['hosts']:
         return json_error("Invalid host")
 
-    return json_from_url("http://" + hosts[host] + "/json/week")
+    return json_from_url("http://" + config['hosts'][host] + "/json/week")
 
 
 @app.route("/<host>/json/month")
 def json_month_str(host):
-    if host not in hosts:
+    if host not in config['hosts']:
         return json_error("Invalid host")
 
-    return json_from_url("http://" + hosts[host] + "/json/month")
+    return json_from_url("http://" + config['hosts'][host] + "/json/month")
 
 
 @app.route("/<host>")
 def summary(host):
-    if host not in hosts:
+    if host not in config['hosts']:
         return render_template("error.html", error_string="Error: Invalid Host.")
 
     return render_template("status.html", host=host)
@@ -75,7 +95,7 @@ def summary(host):
 
 @app.route("/<host>/plots")
 def plots(host):
-    if host not in hosts:
+    if host not in config['hosts']:
         return render_template("error.html", error_string="Error: Invalid Host.")
 
     return render_template("plots.html", host=host)
@@ -83,4 +103,4 @@ def plots(host):
 
 @app.route("/")
 def list():
-    return render_template("hosts.html", hosts=hosts.keys())
+    return render_template("hosts.html", hosts=config['hosts'].keys())
