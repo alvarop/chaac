a27c0d6f15a02ca4fa070e9fc69217672ae8ccde
Alvaro Prieto <alvaro@Alvaros-MacBook-Pro.local>
2019-09-23 16:28:51 -0700

    Using the correct path this time

--- sw/chaac_cache/support/start.sh
+++ sw/chaac_cache/support/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export PIPENV_VENV_IN_PROJECT=true
-pipenv run python -u chaac_cache.py --db /etc/chaac_cfg.yml
+pipenv run python -u chaac_cache.py --db /etc/chaac/chaac_cfg.yml
--- sw/chaac_server/support/start.sh
+++ sw/chaac_server/support/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export PIPENV_VENV_IN_PROJECT=true
-export CONFIG=/etc/chaac_cfg.yml
+export CONFIG=/etc/chaac/chaac_cfg.yml
 pipenv run gunicorn --workers 5 --bind unix:chaac_server.sock -m 007 src:app
