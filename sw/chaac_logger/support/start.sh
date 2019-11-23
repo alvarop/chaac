f348db74dc0873cee305704ba6a6266b54983438
Alvaro Prieto <source@alvaroprieto.com>
2019-11-23 13:55:47 -0800

    use python3

--- sw/chaac_logger/support/start.sh
+++ sw/chaac_logger/support/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pipenv run python -u chaac_logger.py --port /dev/ttyS0 --baud_rate 57600 --db /home/pi/chaac.db
+python3 -u chaac_logger.py --port /dev/ttyS0 --baud_rate 57600 --db /home/pi/chaac.db
