1bf4c006956bcff10495dd6f68e27b4788e656f2
Alvaro Prieto <source@alvaroprieto.com>
2020-06-20 12:16:27 -0700

    Adding blinky usb app

---
+++ fw/chaac/apps/blinky_usb/src/main.c
@@ -0,0 +1,65 @@
+#include <assert.h>
+#include <string.h>
+
+#include "sysinit/sysinit.h"
+#include "os/os.h"
+#include "console/console.h"
+#include "bsp/bsp.h"
+#include "hal/hal_gpio.h"
+
+
+#define BLINK_TASK_PRI         (99)
+#define BLINK_STACK_SIZE       (64)
+struct os_task blink_task;
+os_stack_t blink_task_stack[BLINK_STACK_SIZE];
+
+void blink_task_fn(void *arg) {
+    uint8_t count = 0;
+
+    hal_gpio_init_out(STATUS_LED_PIN, 0);
+    hal_gpio_init_out(TX_LED_PIN, 0);
+    hal_gpio_init_out(RX_LED_PIN, 0);
+ 
+    while(1) {
+        os_time_delay(OS_TICKS_PER_SEC/4);
+        if (count & 1) {
+            hal_gpio_toggle(STATUS_LED_PIN);
+        }
+        
+        if (count & 2) {
+            hal_gpio_toggle(LED_BLINK_PIN);
+        }
+
+        if (count & 1) {
+            hal_gpio_toggle(LED_BLINK_PIN);
+        }
+
+        count++;
+    }
+
+}
+
+int
+main(int argc, char **argv)
+{
+    sysinit();
+
+    os_task_init(
+        &blink_task,
+        "blink_task",
+        blink_task_fn,
+        NULL,
+        BLINK_TASK_PRI,
+        OS_WAIT_FOREVER,
+        blink_task_stack,
+        BLINK_STACK_SIZE);
+
+    while(1) {
+        os_eventq_run(os_eventq_dflt_get());
+    }
+
+    assert(0);
+
+    return 0;
+}
+
