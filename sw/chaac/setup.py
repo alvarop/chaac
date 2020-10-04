4c6609b8df754209c3271334e63566afe5b90997
Alvaro Prieto <source@alvaroprieto.com>
2020-10-04 12:31:37 -0700

    Forgot to update setup.py :(

--- sw/chaac/setup.py
+++ sw/chaac/setup.py
@@ -1,17 +1,29 @@
 """
-Testing
+Chaac library
 """
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="chaac",
     version="0.1",
-    description="Chaac python libraries",
+    description="Chaac Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/alvarop/chaac",
     author="Alvaro Prieto",
     author_email="source@alvaroprieto.com",
     license="MIT",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     include_package_data=True,
     zip_safe=False,
+    python_requires='>=3.6'
 )
+
