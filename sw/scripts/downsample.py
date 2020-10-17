7210ebd6645acba58dd84d89b5645bcc86c412ea
Alvaro Prieto <alvaro@Alvaros-MacBook-Pro.local>
2020-10-17 18:36:11 -0400

    Update downsample to fix pressure data

--- sw/scripts/downsample.py
+++ sw/scripts/downsample.py
@@ -6,10 +6,24 @@ Downsample old db file
 import argparse
 import os
 
+import collections
+
 import sys
 import time
 import json
 from chaac.chaacdb import ChaacDB
+WXRecord = collections.namedtuple("WXRecord", [ "id", "timestamp",
+    "uid",
+    "temperature",
+    "humidity",
+    "pressure",
+    "temperature_in",
+    "light",
+    "battery",
+    "rain",
+    "wind_speed",
+    "wind_dir",
+    "solar_panel"])
 
 parser = argparse.ArgumentParser()
 
@@ -25,10 +39,19 @@ if args.out_db:
 
 rows = in_db.get_records("day")
 in_db.close()
+idx = 1
 for row in rows:
-    print(row)
+    row = row._asdict()
+    row["id"] = idx
+    if row["pressure"] > 1500:
+        row["pressure"] = int((row["pressure"] * 100) - 100000.0)
+        if row["pressure"] & 0x8000:
+            row["pressure"] =  (row["pressure"] - 0x10000)
+        row["pressure"] = (float(row["pressure"]) + 100000.0)/100.0
+    row = WXRecord(**row)
     if args.out_db:
         out_db.add_record(row, row.timestamp, commit=False)
+    idx += 1
 
 if args.out_db:
     out_db.close()
