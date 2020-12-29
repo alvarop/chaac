95a5181edc32ec713a2b5c153f3c35e9f5b7064b
Alvaro Prieto <source@alvaroprieto.com>
2020-12-29 10:38:37 -0800

    Whoops. Fixed debug printf

--- fw/chaac_new/src/lib/debug/debug.c
+++ fw/chaac_new/src/lib/debug/debug.c
@@ -7,7 +7,7 @@ void vDebugInit(){
 
 void _putchar(char cChar) {
   // Wait until ready to transmit
-  while(LL_USART_IsActiveFlag_TXE(USART2)){};
+  while(!LL_USART_IsActiveFlag_TXE(USART2)){};
 
   LL_USART_TransmitData8(USART2, (uint8_t)cChar);
 }
