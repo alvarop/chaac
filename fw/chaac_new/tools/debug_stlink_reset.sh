4af7dd8b290dc169468c7150cb649aa6d349c158
Alvaro Prieto <source@alvaroprieto.com>
2021-03-28 08:54:20 -0700

    Make sure we can reset in LPM and added debug_reset

--- fw/chaac_new/tools/debug_stlink_reset.sh
+++ fw/chaac_new/tools/debug_stlink_reset.sh
@@ -3,7 +3,7 @@
 TOOLS_DIR=`git rev-parse --show-toplevel`/fw/chaac_new/tools
 
 set -m
-openocd -f $TOOLS_DIR/stlink-mod.cfg -f $TOOLS_DIR/st_nucleo_l4.cfg -c init -c halt > /dev/null 2>&1 &
+openocd -f $TOOLS_DIR/stlink-mod.cfg -f $TOOLS_DIR/st_nucleo_l4_hardreset.cfg -c init -c reset -c halt > /dev/null 2>&1 &
 openocd_pid=$!
 set +m
 
